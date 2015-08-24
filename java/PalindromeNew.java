70cc9575a111be9095d49b21f3f9c80f7e2ee4f8
Amaan <amaan.cheval@gmail.com>
2015-08-24 10:43:56 +0530

    Added alternative Palindrome program

---
+++ java/PalindromeNew.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class PalindromeNew {
+    public static void main(String[] args) {
+        String inp;
+        Scanner sc = new Scanner(System.in);
+        inp = sc.next();
+        Boolean isPalindrome = true;
+        int i = 0;
+        int len = inp.length();
+
+        while (i < len / 2) {
+            if (inp.charAt(i) != inp.charAt(len - i - 1)) {
+                isPalindrome = false;
+            }
+            i++;
+        }
+
+        System.out.println("Palindrome: " + isPalindrome);
+    }
+}
