7c4c002fdd213e993bf89ad91c16e5ae71be15b9
Amaan Cheval <amaan.cheval@gmail.com>
2015-08-31 20:52:16 +0530

    Cleaned up a bit

--- c/binarySearch.c
+++ c/binarySearch.c
@@ -4,11 +4,10 @@ int binarySearch(int arr[], int start, int end, int val) {
     int pos = -1; // Return -1 for position if it was not found
     int mid = (start + end) / 2;
 
-    if (end - start <= 0) {
+    if (end == start) {
         return pos;
     }
 
-    // printf("Middle: %d\n", arr[mid]);
     if (arr[mid] == val) {
         return mid;
     }
@@ -25,7 +24,7 @@ int main() {
     int a[] = {1, 3, 4, 5, 8, 10};
     int len = sizeof(a) / sizeof(a[0]);
 
-    int val = 11;
+    int val = 3;
     int pos = binarySearch(a, 0, len, val);
 
     printf("Found %d at %d\n", val, pos);
