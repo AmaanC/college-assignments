f1a57e480240c06f7ca9929ef5f2b257c395953f
Amaan Cheval <amaan.cheval@gmail.com>
2015-09-08 12:32:54 +0530

    Infix to postfix works now

--- c/infixToPostfix.c
+++ c/infixToPostfix.c
@@ -49,11 +49,45 @@ int operand(char c) {
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
 }
 
+int getValueForOperator(char op) {
+    // Using BODMAS
+    int val = 0;
+    switch(op) {
+        case ')':
+            val = 5;
+            break;
+        case '/':
+            val = 4;
+            break;
+        case '*':
+            val = 3;
+            break;
+        case '+':
+            val = 2;
+            break;
+        case '-':
+            val = 1;
+            break;
+        defauult:
+            val = 0;
+            break;
+    }
+    return val;
+};
+
+int higherPrecedence(char op1, char op2) {
+    // Returns 1 if op1 has higher precedence. 0 otherwise
+    int val1 = getValueForOperator(op1);
+    int val2 = getValueForOperator(op2);
+    return val1 > val2;
+}
+
 int main() {
-    char eqn[] = "a+b*c";
-    char result[10] = "ab";
+    char eqn[] = "(a+b)*(c+d)";
+    char result[10] = "";
     int eqI = 0, resI = 0;
     char c;
+    char temp;
 
     initialize(&s);
     // push(&s, 'a');
@@ -69,11 +103,52 @@ int main() {
             result[resI] = c;
             resI++;
         }
-        else {
-            // Push operator to stack
+        else if (c == '(') {
             push(&s, c);
         }
+        else {
+            // It is an operator
+            if (s.top < 0 || seeTop(&s) == '(') {
+                // If the stack is empty, or if there's a ( on the top or if the current operator
+                // has a higher precedence than the top of the stack, push the current operator
+                push(&s, c);
+            }
+            else if (higherPrecedence(c, seeTop(&s)) == 1) {
+                if (c != ')') {
+                    push(&s, c);
+                }
+            }
+            else {
+                while (higherPrecedence(c, seeTop(&s)) == 0) {
+                    printf("Popping: %c\n", seeTop(&s));
+                    result[resI] = pop(&s);
+                    resI++;
+                }
+                push(&s, c);
+            }
+
+            if (c == ')') {
+                do {
+                    temp = pop(&s);
+                    if (temp != '(') {
+                        result[resI] = temp;
+                        resI++;
+                    }
+                }
+                while (temp != '(');
+            }
+        }
     }
-    //printf("%s", result);
+    // Pop all the elements remaining
+    while (s.top >= 0) {
+        result[resI] = pop(&s);
+        resI++;
+    }
+    printf("%s\n", result);
+    /*
+    for (resI = 0; resI < MAX; resI++) {
+        printf("%c, ", s.arr[resI]);
+    }
+    */
     return 0;
 }
